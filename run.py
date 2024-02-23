aa5eaa08a876876abcb66dd1aa1658b88644fb90
marliesgoes <marlies.goes@icloud.com>
2024-02-22 19:00:44 -0500

    add infinite loop and RECORDING_DURATION constant

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from audio_functions import AudioManager
 from utils import call_gpt, print_robot, print_user
 
+RECORDING_DURATION = 30
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: run.py <PERSONA>")
@@ -30,27 +32,28 @@ def main():
     # The AudioManager class holds functions for ASR & TTS
     am = AudioManager()
 
-    # Record and save the user's speech
-    audio = am.record_audio(duration=5) # TODO: Make longer
-    audio_path = am.save_audio(audio)
+    while True:
+        # Record and save the user's speech
+        audio = am.record_audio(duration=RECORDING_DURATION)
+        audio_path = am.save_audio(audio)
 
-    # Transcribe the user's speech
-    transcribed_audio = am.transcribe_audio(audio_path)
-    print_user(transcribed_audio)
+        # Transcribe the user's speech
+        transcribed_audio = am.transcribe_audio(audio_path)
+        print_user(transcribed_audio)
 
-    gpt_prompt = f"""
-        You are an character with the following persona: {personal_prompt}.
-        You are supposed to respond to the following discussion: {transcribed_audio}.
-    """
+        gpt_prompt = f"""
+            You are an character with the following persona: {personal_prompt}.
+            You are supposed to respond to the following discussion: {transcribed_audio}.
+        """
 
-    messages = [{
-        'role': 'user',
-        'content': gpt_prompt
-    }]
+        messages = [{
+            'role': 'user',
+            'content': gpt_prompt
+        }]
 
-    comment = call_gpt(client, messages, "gpt-4-1106-preview")
-    print_robot(comment)
-    am.stream_and_play(comment, voice_id)
+        comment = call_gpt(client, messages, "gpt-4-1106-preview")
+        print_robot(comment)
+        am.stream_and_play(comment, voice_id)
 
 
 if __name__ == "__main__":
